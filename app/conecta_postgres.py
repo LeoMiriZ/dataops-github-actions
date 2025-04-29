b33b370ee6af724cd35c038b95e50a97d212a6a9
LeoMiriZ <le.mbaptista@gmail.com>
2025-04-28 21:09:29 -0300

    primeiro commit - arquivo python

---
+++ app/conecta_postgres.py
@@ -0,0 +1,34 @@
+import psycopg2
+import time
+from flask import Flask
+from flask_restx import Api, Resource
+
+app = Flask(__name__)
+api = Api(app, version='1.0', title='Conecta Postgres API', description='API para conectar ao PostgreSQL', doc='/swagger')
+
+ns = api.namespace('alunos', description='Operações de conexão com o PostgreSQL')
+
+def connect_to_postgres():
+    conn = psycopg2.connect(
+         host="db",
+         dbname="postgres",
+         user="postgres",
+         password="123456"
+    )
+    return conn
+
+@ns.route('/')
+class AlunosList(Resource):
+    def get(self):
+        time.sleep(5)
+        conn = connect_to_postgres()
+        cursor = conn.cursor()
+        cursor.execute("CREATE TABLE IF NOT EXISTS alunos (id SERIAL PRIMARY KEY, nome VARCHAR(100), idade INT)")
+        conn.commit()
+        cursor.execute("INSERT INTO alunos (nome, idade) VALUES (%s, %s)", ('João', 20))
+        conn.commit()
+        cursor.execute("SELECT * FROM alunos")
+        alunos = cursor.fetchall()
+        cursor.close()
+        conn.close()
+        return {'alunos': [{'id': aluno[0], 'nome': aluno[1], 'idade': aluno[2]} for aluno in alunos]}
